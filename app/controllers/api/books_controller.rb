90d3c9f78f76b2f800966c6f090782ac41a51072
mufazzal hussain <mufazzalh.cuelogic@gmail.com>
2014-04-18 17:35:27 +0530

    webservices built for rendering index file and table of content from epub for a perticular book_id

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -12,17 +12,21 @@ class Api::BooksController < ApplicationController
 		File.open(file_path, "r") {|file|  @toc = Nokogiri::HTML(file.read)} 
 		render json: jsonify_toc(@toc), status: 200
 	end
+
+	private
+
+	def jsonify_toc(toc)
+		toc_json = {}
+		count = 0
+		toc.css('navpoint').each do |navpoint|	
+			id = navpoint.attributes["id"].value	
+			text = navpoint.at_css('text').text
+			src = navpoint.at_css('content').attributes['src'].value
+			toc_json.merge!(count.to_s => { "id" => id, "text" => text, "src" => src})
+			count += 1
+		end
+		toc_json
+	end
 end
 
-def jsonify_toc(toc)
-	toc_json = {}
-	count = 0
-	toc.css('navpoint').each do |navpoint|	
-		id = navpoint.attributes["id"].value	
-		text = navpoint.at_css('text').text
-		src = navpoint.at_css('content').attributes['src'].value
-		toc_json.merge!(count.to_s => { "id" => id, "text" => text, "src" => src})
-		count += 1
-	end
-	toc_json
-end
+
