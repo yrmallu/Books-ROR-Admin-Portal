b8f18876d8987c3991ff2a18bcc89a0ed674f373
Kalyani Bagale <kalyani.bagale@cuelogic.co.in>
2014-04-18 11:06:33 +0530

    changes in production.rb and database.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,21 +24,21 @@ BooksThatGrow::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
+  config.assets.css_compressor = :sass
+  config.assets.precompile += %w( *.css *.js )
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
-
+  config.serve_static_assets = true
   # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
+  config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  #config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
