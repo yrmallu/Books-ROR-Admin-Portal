97fad73fba4d4169af8df346a0e658dd854dc9cf
7 hills <yedukondala.reddy@cuelogic.co.in>
2014-05-20 11:05:54 +0530

    Reset password related tasks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,20 +78,20 @@ BooksThatGrow::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  config.action_mailer.default_url_options = {:host => 'books-that-grow.herokuapp.com'} 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true 
-  config.action_mailer.raise_delivery_errors = true 
-  config.action_mailer.default :charset => "utf-8"
-
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "books-that-grow.herokuapp.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
-  }
+  # config.action_mailer.default_url_options = {:host => 'books-that-grow.herokuapp.com'} 
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true 
+  # config.action_mailer.raise_delivery_errors = true 
+  # config.action_mailer.default :charset => "utf-8"
+  # 
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: "books-that-grow.herokuapp.com",
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: ENV["GMAIL_USERNAME"],
+  #   password: ENV["GMAIL_PASSWORD"]
+  # }
 	
 end
