43a8316fabb17c9e10c14a7ea1f65afe83c89289
7 hills <yedukondala.reddy@cuelogic.co.in>
2014-05-27 18:46:04 +0530

    API issue

--- app/controllers/api/resetpasswords_controller.rb
+++ app/controllers/api/resetpasswords_controller.rb
@@ -14,9 +14,9 @@ class Api::ResetpasswordsController < ApplicationController
       s_parents = []
       unless @user.blank?
         json_data = {"return_code" => 1, "return_msg" => "linked email addresses for student"}
-        teachers = {}
         @user.classrooms.each do |room|
           room.users.un_archived.teachers.each do |teacher|
+            teachers = {}
             teachers.store("id",teacher.id)
             teachers.store("name",teacher.first_name+" "+teacher.last_name.to_s)
             teachers.store("email",teacher.email)
