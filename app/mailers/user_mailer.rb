8fd6e64824520930faf5adec5b422e4404d0449a
Kalyani Bagale <kalyani.bagale@cuelogic.co.in>
2014-05-28 13:19:54 +0530

    removed unnecessary objs for mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,39 +2,42 @@ class UserMailer < ActionMailer::Base
   default from: 'noreply@borne-digital.com'
   
   def welcome_email(user_info)
-    @email = user_info[:email]
-	@firstname = user_info[:firstname]
-	#@lastname = user_info[:lastname]
-	@username = user_info[:username]
-    @link =  user_info[:link]
-    @login_url  = user_info[:login_url]
-    @reset_pass_url = user_info[:reset_pass_url]
-    mail(to: @email, subject: 'Welcome to Books That Grow!')
+    @user_info = user_info
+    #@email = user_info[:email]
+	  # @firstname = user_info[:firstname]
+	  # @lastname = user_info[:lastname]
+	  # @username = user_info[:username]
+   #  @link =  user_info[:link]
+   #  @login_url  = user_info[:login_url]
+   #  @reset_pass_url = user_info[:reset_pass_url]
+    mail(to: @user_info[:email], subject: 'Welcome to Books That Grow!')
   end
 
   def user_details_changed(user_info)
-    @email = user_info[:email]
-    @changed_attributes = user_info[:changed_attributes]
-    @username = user_info[:username]
-	@name = user_info[:name]
-    @logged_in_user = user_info[:current_user]
-    @link =  user_info[:link]
-    @login_url  = user_info[:login_url]
-    @reset_pass_url = user_info[:reset_pass_url]
-    mail(to: @email, subject: 'Your Books That Grow account has been updated.')
+    @user_info = user_info
+   #  @email = user_info[:email]
+   #  @changed_attributes = user_info[:changed_attributes]
+   #  @username = user_info[:username]
+	  # @name = user_info[:name]
+   #  @logged_in_user = user_info[:current_user]
+   #  @link =  user_info[:link]
+   #  @login_url  = user_info[:login_url]
+   #  @reset_pass_url = user_info[:reset_pass_url]
+    mail(to: @user_info[:email], subject: 'Your Books That Grow account has been updated.')
   end
 
   def user_email_changed(user_info)
-    @email = user_info[:email]
-    @changed_attributes = user_info[:changed_attributes]	
-    @username = user_info[:username]
-	@name = user_info[:name]
-    @logged_in_user = user_info[:current_user]
-    @new_email = user_info[:new_email]
-    @link =  user_info[:link]
-    @login_url  = user_info[:login_url]
-    @reset_pass_url = user_info[:reset_pass_url]
-    mail(to: @email, subject: 'Books That Grow Account Email Address changed.')
+    @user_info = user_info
+   #  @email = user_info[:email]
+   #  @changed_attributes = user_info[:changed_attributes]	
+   #  @username = user_info[:username]
+	  # @name = user_info[:name]
+   #  @logged_in_user = user_info[:current_user]
+   #  @new_email = user_info[:new_email]
+   #  @link =  user_info[:link]
+   #  @login_url  = user_info[:login_url]
+   #  @reset_pass_url = user_info[:reset_pass_url]
+    mail(to: @user_info[:email], subject: 'Books That Grow Account Email Address changed.')
   end
 
   def forgot_password_email(user_info)
