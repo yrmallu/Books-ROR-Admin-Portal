6b1da60309e6475fc729161e8e6691202be81735
Kalyani Bagale <kalyani.bagale@cuelogic.co.in>
2014-06-30 18:52:18 +0530

    resolved issues resolved to classroom/user multiselect

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,23 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.smtp_settings = {  
-      :address              => "smtpout.secureserver.net",  
-      :port                 => 465,  
-      :domain               => "booksthatgrow.com",  
-      :user_name            => "noreply@booksthatgrow.com",
-      :password             => "bornedigital",
+      :address              => "smtp.gmail.com",  
+      :port                 => 587,  
+      :domain               => "gmail.com",  
+      :user_name            => "cuelogic.test.acc@gmail.com",
+      :password             => "cuelogic",
       :authentication       => "plain",  
       :enable_starttls_auto => true  
     }
+
+
+    # ActionMailer::Base.smtp_settings = {  
+    #   :address              => "smtpout.secureserver.net",  
+    #   :port                 => 465,  
+    #   :domain               => "booksthatgrow.com",  
+    #   :user_name            => "noreply@booksthatgrow.com",
+    #   :password             => "bornedigital",
+    #   :authentication       => "plain",  
+    #   :enable_starttls_auto => true  
+    # }
 
