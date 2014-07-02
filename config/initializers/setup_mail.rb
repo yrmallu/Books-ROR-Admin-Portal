ca204956796d0bb24f2c57c9fc419ec3a517b759
Kalyani Bagale <kalyani.bagale@cuelogic.co.in>
2014-07-02 15:34:12 +0530

    added new email for mails

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,23 +1,24 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.raise_delivery_errors = true
+# ActionMailer::Base.smtp_settings = {  
+#       :address              => "smtp.gmail.com",  
+#       :port                 => 587,  
+#       :domain               => "gmail.com",  
+#       :user_name            => "cuelogic.test.acc@gmail.com",
+#       :password             => "cuelogic",
+#       :authentication       => "plain",  
+#       :enable_starttls_auto => true  
+#     }
+
+display_name = "Books That Grow Support"
 ActionMailer::Base.smtp_settings = {  
-      :address              => "smtp.gmail.com",  
-      :port                 => 587,  
-      :domain               => "gmail.com",  
-      :user_name            => "cuelogic.test.acc@gmail.com",
-      :password             => "cuelogic",
-      :authentication       => "plain",  
-      :enable_starttls_auto => true  
-    }
-
-
-    # ActionMailer::Base.smtp_settings = {  
-    #   :address              => "smtpout.secureserver.net",  
-    #   :port                 => 465,  
-    #   :domain               => "booksthatgrow.com",  
-    #   :user_name            => "noreply@booksthatgrow.com",
-    #   :password             => "bornedigital",
-    #   :authentication       => "plain",  
-    #   :enable_starttls_auto => true  
-    # }
+    :address              => "smtpout.secureserver.net",  
+    :port                 => 80,  
+    :domain               => "booksthatgrow.com",  
+    #:user_name            => "\"#{display_name}\" <noreply@booksthatgrow.com>",
+    :user_name            => "noreply@booksthatgrow.com",
+    :password             => "bornedigital",
+    :authentication       => "plain",  
+    :enable_starttls_auto => true      
+}
 
