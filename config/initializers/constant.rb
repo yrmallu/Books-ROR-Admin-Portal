b2a8b2e71815b2204d00292143dab0fd1589ad8c
Kalyani Bagale <kalyani.bagale@cuelogic.co.in>
2014-07-15 16:32:46 +0530

    allow hypens,apostrope for fname,lname

--- config/initializers/constant.rb
+++ config/initializers/constant.rb
@@ -1,4 +1,4 @@
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   NO_SPACE_REGEX = /\A\S+\Z/
-  LETTER_ONLY_REGEX = /\A(([a-zA-Z])+(-?[a-zA-Z]+)*\s?)+\Z/
-  
+ # LETTER_ONLY_REGEX = /\A(([a-zA-Z])+(-?[a-zA-Z]+)*\s?)+\Z/  
+  LETTER_ONLY_REGEX =  /\A(([a-zA-Z\s][a-zA-Z\s'.-]*[a-zA-Z'.\s])|([a-zA-Z\s]))\Z/ 
